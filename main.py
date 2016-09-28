4a1b114513e1664b5cb2c8bc36581a7b4f37ae6d
Brian Kimmel <kimmelb@gmail.com>
2016-09-28 18:46:23 -0400

    Adding sudoku generation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PySide.QtGui import *
 from PySide.QtCore import *
 from ui_mainWindow import *
+from lib import build_sudoku_pdf
 
 class MainWindow(QMainWindow, Ui_MainWindow):
    def __init__(self):
@@ -11,7 +12,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
    
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   mainWin = MainWindow()
-   ret = app.exec_()
-   sys.exit( ret )
+   	build_sudoku_pdf.GeneratePDF(1, 1, 1, "Easy", False)
+
+	app = QApplication(sys.argv)
+	mainWin = MainWindow()
+	ret = app.exec_()
+
+
+	sys.exit( ret )

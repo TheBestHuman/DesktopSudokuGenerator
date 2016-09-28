b036cd722c694852ab7bd9db293c22c292c72043
Brian Kimmel <kimmelb@gmail.com>
2016-09-28 19:04:43 -0400

    Button triggers the generator

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from ui_mainWindow import *
 from lib import build_sudoku_pdf
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+	self.sudoku_worker = SudokuWorker()
+	self.sudoku_worker.updateProgress.connect(self.setProgress)
+
 	def __init__(self):
 		super(MainWindow, self).__init__()
 		self.setupUi(self)
@@ -14,9 +17,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.show()     
 	
 	def Go(self):
-		build_sudoku_pdf.GeneratePDF(1, 1, 1, "Easy", False)
+		self.sudoku_worker.start()
 
-   
+	def setProgress(self, progress):
+		self.pushButton.setText(progress)
+
+
+
+#Inherit from QThread
+class SudokuWorker(QtCore.QThread):
+
+    #This is the signal that will be emitted during the processing.
+    #By including int as an argument, it lets the signal know to expect
+    #an integer argument when emitting.
+    updateProgress = QtCore.Signal(int)
+
+    #You can do any extra things in this init you need, but for this example
+    #nothing else needs to be done expect call the super's init
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+
+    #A QThread is run by calling it's start() function, which calls this run()
+    #function in it's own "thread". 
+    def run(self):
+        build_sudoku_pdf.GeneratePDF(1, 1, 1, "Easy", False)
+        self.updateProgress.emit(1)
 
 if __name__ == '__main__':
    	

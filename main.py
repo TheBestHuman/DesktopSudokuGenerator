dcd87963bccd434b12a2626839bec8b56a058463
Brian Kimmel <kimmelb@gmail.com>
2016-09-28 18:59:06 -0400

    Button triggers the generator

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from ui_mainWindow import *
 from lib import build_sudoku_pdf
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-   def __init__(self):
-       super(MainWindow, self).__init__()
-       self.setupUi(self)
-       self.show()
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.setupUi(self)
+
+		self.pushButton.clicked.connect(self.Go)
+
+		self.show()     
+	
+	def Go(self):
+		build_sudoku_pdf.GeneratePDF(1, 1, 1, "Easy", False)
+
    
 
 if __name__ == '__main__':
-   	build_sudoku_pdf.GeneratePDF(1, 1, 1, "Easy", False)
+   	
 
 	app = QApplication(sys.argv)
 	mainWin = MainWindow()

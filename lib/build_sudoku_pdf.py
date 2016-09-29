3fde936946bc673f26f068d6196a5ad5423893fe
Brian Kimmel <kimmelb@gmail.com>
2016-09-28 21:28:35 -0400

    adding four-per-page functionality

--- lib/build_sudoku_pdf.py
+++ lib/build_sudoku_pdf.py
@@ -28,70 +28,133 @@ styles = getSampleStyleSheet()
 ParaStyle = styles["Normal"]
 
 def renderPuzzle(page, puzzle, top, left, size, fontSize=24, thinLine=1, thickLine=4):
-    right = left + size
-    bottom = top - size
-    page.saveState()
+	right = left + size
+	bottom = top - size
+	page.saveState()
 
-    box_height = size / 9
-    for i in range(0, 10):
-        if i in [0, 3, 6, 9]:
-            page.setLineWidth(thickLine)
-        else:
-            page.setLineWidth(thinLine)
-        page.line(left, top - i * box_height, right, top - i * box_height)
-        page.line(left + i * box_height, top, left + i * box_height, bottom)
+	box_height = size / 9
+	for i in range(0, 10):
+		if i in [0, 3, 6, 9]:
+			page.setLineWidth(thickLine)
+		else:
+			page.setLineWidth(thinLine)
+		page.line(left, top - i * box_height, right, top - i * box_height)
+		page.line(left + i * box_height, top, left + i * box_height, bottom)
 
-    s = puzzle.to_string()
-    nums = s.split()
-    #print puzzle
-    page.setFont('Times-Bold',fontSize)
-    for row in range(0, 9):
-        for col in range(0, 9):
-            num = nums[row * 9 + col]
-            if num != '0':
-                page.drawString(left + col * box_height + box_height * 0.38, top - row * box_height - box_height * 0.65, num)
-    page.restoreState()
+	s = puzzle.to_string()
+	nums = s.split()
+	#print puzzle
+	page.setFont('Times-Bold',fontSize)
+	for row in range(0, 9):
+		for col in range(0, 9):
+			num = nums[row * 9 + col]
+			if num != '0':
+				page.drawString(left + col * box_height + box_height * 0.38, top - row * box_height - box_height * 0.65, num)
+	page.restoreState()
 
 def generatePage(puzzle, dpuz, page, puzzlenum, difficulty='Any', showFooter=True):
 
-    puz = puzzle
-    d = dpuz
+	puz = puzzle
+	d = dpuz
 
+	top = PAGE_HEIGHT
+	left = 36
+	renderPuzzle(page, puz, top - 72 * 2, left, PAGE_WIDTH - 72, fontSize=24)
+
+	page.setFont('Times-Bold',12)
+	page.drawString(left, top - 72 * 1.5, "Puzzle %d (%s, difficulty rating %.02f)" % (puzzlenum, d.value_string(), d.value))
+	#if showFooter:
+	#    generateFooter(page)
+	#return puz, d
+def generateFourUpPage(puzzles, page, puzzlenum, difficulty='Any'):
+
+    inch = 72
     top = PAGE_HEIGHT
     left = 36
-    renderPuzzle(page, puz, top - 72 * 2, left, PAGE_WIDTH - 72, fontSize=24)
+    size = (PAGE_WIDTH - inch * 1.5) / 2
+    coords = [
+        (top - inch * 1, left),
+        (top - inch * 1, left + size + inch * .5),
+        (top - inch * 1 - size - inch, left),
+        (top - inch * 1 - size - inch, left + size + inch * .5),
+    ]
 
-    page.setFont('Times-Bold',12)
-    page.drawString(left, top - 72 * 1.5, "Puzzle %d (%s, difficulty rating %.02f)" % (puzzlenum, d.value_string(), d.value))
-    #if showFooter:
-    #    generateFooter(page)
-    #return puz, d
+    i = 0
+    for puzzle in puzzles:
+        puz, d = puzzle
+        renderPuzzle(page, puz, coords[i][0], coords[i][1], size, fontSize=16)
+        page.setFont('Times-Bold',12)
+        page.drawString(coords[i][1], coords[i][0] + inch * 0.25, "Puzzle %d (%s, difficulty rating %.02f)" % (puzzlenum + i, d.value_string(), d.value))
+        i += 1
+
+    return puz, d
+
+def generateSolutions(page, puzzles):
+    col = row = 0
+    i = 0
+    for puz, d in puzzles:
+        i += 1
+        page.setFont('Times-Bold',8)
+        page.drawString(36 + col * 72 * 2.5, PAGE_HEIGHT - 72 - row * 72 * 2.5 + 6, "Puzzle %d (%s, difficulty rating %.02f)" % (i, d.value_string(), d.value))
+        solver = sudoku.SudokuRater(puz.grid,verbose=False, group_size=puz.group_size)
+        solver.solve()
+        renderPuzzle(page, solver, PAGE_HEIGHT - 72 - row * 72 * 2.5, 36 + col * 72 * 2.5, 72 * 2, fontSize=10, thickLine=2)
+        col += 1
+        if col == 3:
+            col = 0
+            row += 1
+            if row == 4:
+                col = row = 0
+                page.showPage()
 
 def GeneratePDF(progress, total_puzzles, puzzles_per_page, pages_per_pdf, difficulty, include_solutions, outputdirectory = "."):
 
-    progress.updateProgress.emit(.1)
-    output_filename = "Sudoku_" + difficulty + "_" + time.strftime("%Y%m%d-%H%M%S") + ".pdf"
-    doc = canvas.Canvas(filename=output_filename, pagesize=defaultPageSize)
+	progress.updateProgress.emit(0)
+	progress_increment = 100/total_puzzles
+	current_progress = 0
 
-    g = sudoku_maker.SudokuGenerator()
-    j = 0
-    puzzlelist = []
-    while (j <  total_puzzles):
-    	puzzles = g.make_unique_puzzles(1)
-    	#print puzzles[0][1].value_string()
-    	if(puzzles[0][1].value_string() == difficulty):
-    		puzzlelist = puzzlelist + puzzles
-    		j = j + 1
-    i = 1
-    for puz, d in puzzlelist:
+	output_filename = "Sudoku_" + difficulty + "_" + time.strftime("%Y%m%d-%H%M%S") + ".pdf"
+	doc = canvas.Canvas(filename=output_filename, pagesize=defaultPageSize)
 
-        generatePage(puz,d, doc, i)
-        doc.showPage()
+	g = sudoku_maker.SudokuGenerator()
+	j = 0
+	puzzlelist = []
+	while (j <  total_puzzles):
+		puzzles = g.make_unique_puzzles(1)
 
-        i = i + 1
-    doc.save()
-     #   fd, outfile = tempfile.mkstemp(dir=os.path.join(DATA_DIR, d.value_string()))
-     #   print outfile
-     #   out = os.fdopen(fd, 'w')
-     #   pickle.dump(puz, out)
-     #   out.close()
+		if puzzles[0][1].value_string() == difficulty :
+			puzzlelist = puzzlelist + puzzles
+			j = j + 1
+			current_progress = current_progress + progress_increment
+			progress.updateProgress.emit(current_progress)
+	i = 1
+
+	if puzzles_per_page == 4:
+		currentpagelist = []
+		for puz, d in puzzlelist:
+
+			currentpagelist.append((puz,d))
+			if(i%4 == 0):
+				generateFourUpPage(currentpagelist, doc, i - (len(currentpagelist)-1))
+				doc.showPage()
+				currentpagelist = []
+
+			i = i + 1
+
+		generateFourUpPage(currentpagelist, doc, i - (len(currentpagelist)))
+		doc.showPage()
+	else:
+		for puz, d in puzzlelist:
+
+			generatePage(puz,d, doc, i)
+			doc.showPage()
+
+			i = i + 1
+	doc.save()
+
+	progress.updateProgress.emit(100)
+	 #   fd, outfile = tempfile.mkstemp(dir=os.path.join(DATA_DIR, d.value_string()))
+	 #   print outfile
+	 #   out = os.fdopen(fd, 'w')
+	 #   pickle.dump(puz, out)
+	 #   out.close()
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class SudokuWorker(QtCore.QThread):
     #A QThread is run by calling it's start() function, which calls this run()
     #function in it's own "thread". 
     def run(self):
-        build_sudoku_pdf.GeneratePDF(self, 1, 1, 1, "Easy", False)
+        build_sudoku_pdf.GeneratePDF(self, 5, 4, 1, "Easy", False)
         self.updateProgress.emit(1)
 
 
@@ -35,15 +35,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 		self.setupUi(self)
 
-		self.pushButton.clicked.connect(self.Go)
+		self.go_button.clicked.connect(self.Go)
+		self.progressBar.hide()
+		self.progressBar.setValue(0);
+		self.progress_label.hide()
 
 		self.show()     
 	
 	def Go(self):
+		self.progressBar.show()
+		self.progressBar.setValue(0)
+		self.progress_label.setText("Beginning file generation process.")
+		self.progress_label.show()
 		self.sudoku_worker.start()
 
 	def setProgress(self, progress):
-		self.pushButton.setText(str(progress))
+		self.progressBar.setValue(progress)
+		if progress == 100:
+			self.progressBar.hide()
+			self.progress_label.setText("File generation complete.")
 
 
 
